fbe1da145de162e1037a1d27eafb4e3231e2684b
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-05-01 22:37:30 +0900

    Update N-Queen.swift

--- dfs/N-Queen.swift
+++ dfs/N-Queen.swift
@@ -1,39 +1,109 @@
 import Foundation
 
-let n = 4
-var chessBoard = Array(repeating: Array(repeating: false, count: n), count: n)
-var resultSet = [(row:Int, column:Int)]()
+let n = 3
+var chessBoard: [[Bool]] { Array(repeating: [Bool](repeating: true, count: n), count: n) }
 var result = 0
 
-for row in 0..<chessBoard.endIndex {
-    for column in 0..<chessBoard.endIndex {
+for row in 0..<n {
+    for column in 0..<n {
         
-        chessBoard[row][column] = true
-        resultSet.append((row: row, column: column))
+        var board = chessBoard
         
-        for testRow in 0..<chessBoard.endIndex {
-            for testColumn in 0..<chessBoard.endIndex {
-                if testRow == row && testColumn == column {
-                    continue
-                } else if resultSet.contains(where: {$0.row == testRow}) {
-                    chessBoard[testRow][testColumn] = false
-                } else if resultSet.contains(where: {$0.column == testColumn}) {
-                    chessBoard[testRow][testColumn] = false
-                } else if testRow == testColumn {
-                    chessBoard[testRow][testColumn] = false
-                } else {
-                    chessBoard[testRow][testColumn] = true
-                    resultSet.append((row: testRow, column: testColumn))
-                }
+        board[row] = board[row].map { _ in false }
+        
+        for innerRow in 0..<n {
+            board[innerRow][column] = false
+        }
+        
+        diagonalCheck(in: row-1, at: column-1, board: &board, direction: .upperLeft)
+        diagonalCheck(in: row-1, at: column+1, board: &board, direction: .upperRight)
+        diagonalCheck(in: row+1, at: column-1, board: &board, direction: .downLeft)
+        diagonalCheck(in: row+1, at: column+1, board: &board, direction: .downRight)
+        
+        board[row][column] = true
+        
+        if column+1 == n {
+            if row+1 == n {
+                continue
             }
+            checkQueen(in: row+1, at: 0, board: &board)
+        } else {
+            checkQueen(in: row, at: column+1, board: &board)
+        }
+    }
+}
+
+func checkQueen(in row: Int, at column: Int, board: inout [[Bool]]) {
+    
+    let queenCount = board.reduce(0, {$0 + $1.filter({$0 == true}).count})
+    
+    if queenCount == n {
+        result += 1
+        return
+    } else if row == n-1 && column == n-1 {
+        return
+    } else if queenCount < n {
+        return
+    }
+    
+    if board[row][column] {
+        
+        board[row] = board[row].map { _ in return false }
+        
+        for innerRow in 0..<n {
+            board[innerRow][column] = false
         }
         
-        if n == chessBoard.reduce(0, { $0 + $1.filter({isQueen in isQueen == true}).count}) {
-            result += 1
+        board[row][column] = true
+        
+        diagonalCheck(in: row-1, at: column-1, board: &board, direction: .upperLeft)
+        diagonalCheck(in: row-1, at: column+1, board: &board, direction: .upperRight)
+        diagonalCheck(in: row+1, at: column-1, board: &board, direction: .downLeft)
+        diagonalCheck(in: row+1, at: column+1, board: &board, direction: .downRight)
+    }
+    
+    if column+1 == n {
+        checkQueen(in: row+1, at: 0, board: &board)
+    } else {
+        checkQueen(in: row, at: column+1, board: &board)
+    }
+}
+
+func diagonalCheck(in row: Int, at column: Int, board: inout [[Bool]], direction: DiagonalDirection) {
+    if row < 0 || column < 0 || row == n || column == n {
+        return
+    }
+    
+    board[row][column] = false
+    
+    diagonalCheck(
+        in: row + direction.moveRow(),
+        at: column + direction.moveColumn(),
+        board: &board,
+        direction: direction
+    )
+}
+
+enum DiagonalDirection {
+    case upperLeft
+    case upperRight
+    case downLeft
+    case downRight
+    
+    func moveRow() -> Int {
+        if self == .upperLeft || self == .upperRight {
+            return -1
         }
         
-        chessBoard = Array(repeating: Array(repeating: false, count: n), count: n)
-        resultSet.removeAll()
+        return 1
+    }
+    
+    func moveColumn() -> Int {
+        if self == .upperLeft || self == .downLeft {
+            return -1
+        }
+        
+        return 1
     }
 }
 
