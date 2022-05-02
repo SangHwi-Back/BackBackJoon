b6d8b3faf332656d968c8ebb8c76218999441ec0
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-05-02 22:24:28 +0900

    Update N-Queen.swift

--- dfs/N-Queen.swift
+++ dfs/N-Queen.swift
@@ -1,87 +1,42 @@
 import Foundation
 
-let n = 3
-var chessBoard: [[Bool]] { Array(repeating: [Bool](repeating: true, count: n), count: n) }
+let n = 4
 var result = 0
 
 for row in 0..<n {
     for column in 0..<n {
         
-        var board = chessBoard
+        let board = ChessBoard(width: n)
+        let point = QueenPoint(in: row, at: column)
         
-        board[row] = board[row].map { _ in false }
-        
-        for innerRow in 0..<n {
-            board[innerRow][column] = false
-        }
-        
-        diagonalCheck(in: row-1, at: column-1, board: &board, direction: .upperLeft)
-        diagonalCheck(in: row-1, at: column+1, board: &board, direction: .upperRight)
-        diagonalCheck(in: row+1, at: column-1, board: &board, direction: .downLeft)
-        diagonalCheck(in: row+1, at: column+1, board: &board, direction: .downRight)
-        
-        board[row][column] = true
-        
-        if column+1 == n {
-            if row+1 == n {
-                continue
-            }
-            checkQueen(in: row+1, at: 0, board: &board)
-        } else {
-            checkQueen(in: row, at: column+1, board: &board)
-        }
+        board.placeQueen(at: point)
+        checkQueen(at: point, board: board)
     }
 }
 
-func checkQueen(in row: Int, at column: Int, board: inout [[Bool]]) {
+func checkQueen(at point: QueenPoint, board: ChessBoard) {
     
-    let queenCount = board.reduce(0, {$0 + $1.filter({$0 == true}).count})
-    
-    if queenCount == n {
-        result += 1
-        return
-    } else if row == n-1 && column == n-1 {
-        return
-    } else if queenCount < n {
+    if point.row == n-1 && point.column == n-1 {
+        result += (board.queenCount == n ? 1 : 0)
         return
     }
     
-    if board[row][column] {
-        
-        board[row] = board[row].map { _ in return false }
-        
-        for innerRow in 0..<n {
-            board[innerRow][column] = false
-        }
-        
-        board[row][column] = true
-        
-        diagonalCheck(in: row-1, at: column-1, board: &board, direction: .upperLeft)
-        diagonalCheck(in: row-1, at: column+1, board: &board, direction: .upperRight)
-        diagonalCheck(in: row+1, at: column-1, board: &board, direction: .downLeft)
-        diagonalCheck(in: row+1, at: column+1, board: &board, direction: .downRight)
-    }
-    
-    if column+1 == n {
-        checkQueen(in: row+1, at: 0, board: &board)
-    } else {
-        checkQueen(in: row, at: column+1, board: &board)
-    }
-}
-
-func diagonalCheck(in row: Int, at column: Int, board: inout [[Bool]], direction: DiagonalDirection) {
-    if row < 0 || column < 0 || row == n || column == n {
+    if board.queenCount > n {
         return
     }
     
-    board[row][column] = false
+    board.setQueen()
     
-    diagonalCheck(
-        in: row + direction.moveRow(),
-        at: column + direction.moveColumn(),
-        board: &board,
-        direction: direction
-    )
+    if board[point] {
+        
+        board.placeQueen(at: point)
+        
+        let newBoard = ChessBoard(width: n)
+        newBoard.queenPosition = board.queenPosition
+        checkQueen(at: point.getNextPoint(), board: newBoard)
+    }
+    
+    checkQueen(at: point.getNextPoint(), board: board)
 }
 
 enum DiagonalDirection {
@@ -94,7 +49,6 @@ enum DiagonalDirection {
         if self == .upperLeft || self == .upperRight {
             return -1
         }
-        
         return 1
     }
     
@@ -102,8 +56,100 @@ enum DiagonalDirection {
         if self == .upperLeft || self == .downLeft {
             return -1
         }
-        
         return 1
+    }
+}
+
+struct QueenPoint: Hashable {
+    let row: Int
+    let column: Int
+    
+    init(in row: Int, at column: Int) {
+        self.row = row
+        self.column = column
+    }
+    
+    func getNextPoint() -> QueenPoint {
+        return QueenPoint(
+            in: row + (column+1 == n ? 1 : 0),
+            at: (column+1 == n ? 0 : column+1)
+        )
+    }
+    
+    func moveDiagonal(_ direction: DiagonalDirection) -> QueenPoint {
+        return QueenPoint(
+            in: row + direction.moveRow(),
+            at: column + direction.moveColumn()
+        )
+    }
+}
+
+class ChessBoard {
+    private var width: Int
+    var queenPosition = Set<QueenPoint>()
+    var chessBoard: [[Bool]]
+    var queenCount: Int {
+        queenPosition.count
+    }
+    
+    subscript(point: QueenPoint) -> Bool {
+        get {
+            return chessBoard[point.row][point.column]
+        }
+        set(newValue) {
+            chessBoard[point.row][point.column] = newValue
+        }
+    }
+    
+    subscript(row: Int) -> [Bool] {
+        get {
+            return chessBoard[row]
+        }
+        set(newValue) {
+            chessBoard[row] = newValue
+        }
+    }
+    
+    init(width: Int) {
+        self.width = width
+        chessBoard = Array(repeating: [Bool](repeating: true, count: width), count: width)
+    }
+    
+    func setQueen() {
+        chessBoard = Array(repeating: [Bool](repeating: true, count: width), count: width)
+        
+        for point in queenPosition {
+            chessBoard[point.row] = chessBoard[point.row].map { _ in return false }
+            
+            for innerRow in 0..<n {
+                chessBoard[innerRow][point.column] = false
+            }
+            
+            diagonalCheck(at: point, direction: .upperLeft)
+            diagonalCheck(at: point, direction: .upperRight)
+            diagonalCheck(at: point, direction: .downLeft)
+            diagonalCheck(at: point, direction: .downRight)
+            
+            chessBoard[point.row][point.column] = true
+        }
+    }
+    
+    func placeQueen(at point: QueenPoint) {
+        queenPosition.insert(point)
+    }
+    
+    func removeQueen(at point: QueenPoint) {
+        queenPosition.remove(point)
+    }
+    
+    private func diagonalCheck(at point: QueenPoint, direction: DiagonalDirection) {
+        if point.row < 0 || point.column < 0 || point.row == width || point.column == width {
+            return
+        }
+        
+        chessBoard[point.row][point.column] = false
+        
+        diagonalCheck(at: point.moveDiagonal(direction), direction: direction)
     }
 }
 
